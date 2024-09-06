9785be3e3d174204be4b9f046039cefcdfdcb350
yalcinmete <yalcinmete.g4@gmail.com>
2024-09-06 17:42:34 +0300

    CoreKatmanında IGenericRepository,IService,IUnitOfWork interfaceleri oluşturuldu.

---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        Task<IEnumerable<T>> GetAllAsync();
+
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        //Servis kullandığım yerlerde SaveChangeAsync() kullanacağım için Update,Remove RemoveRange dönüş tipleerini Task'a çevirdik.GenericRepository'de Task değil void kullanmıştık.
+        Task UpdateAsync(T entity);
+
+        Task RemoveAsync(T entity);
+
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
