765209b74bf6649ee5d2143b36e1940c3a16a828
yalcinmete <yalcinmete.g4@gmail.com>
2024-09-09 15:55:28 +0300

    İlk Migration yapıldı.

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Repositories;
+using NLayer.Core.UnitOfWorks;
+using NLayer.Repository;
+using NLayer.Repository.Repositories;
+using NLayer.Repository.UnitOfWorks;
+using System.Reflection;
+
 namespace NLayer.API
 {
     public class Program
@@ -12,6 +20,21 @@ namespace NLayer.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            //Video80Migrations
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            //IGenericRepository birden çok T almýþ olsaydý : IGenericRepository<,,,> þeklinde yazýcaktýk.
+            builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+
+            //Video80 Migrations.
+            builder.Services.AddDbContext<AppDbContext>(x =>
+            {
+                x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+                {
+                    //option sebebi ; AppDbContext assembly'si API katmanýnda olmadýðý için, AppDbContext'in yerini EF'a söylüyoruz.
+                    option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+                });
+            });
 
             var app = builder.Build();
 
