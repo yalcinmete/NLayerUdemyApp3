ff1c6947f2bca95d0d00d98f222909693e1d144a
yalcinmete <yalcinmete.g4@gmail.com>
2024-09-13 17:22:36 +0300

    AppDbContext override SaveChanges() ; CreatedDate ve UpdateDate düzenlendi

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -26,6 +26,59 @@ namespace NLayer.Repository
         //var p = new Product() {ProductFeature = new ProductFeature(){}}
         public DbSet<ProductFeature> ProductFeatures { get; set; }
 
+
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            {
+                                entityReference.CreatedDate = DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+                                Entry(entityReference).Property(x => x.CreatedDate).IsModified = false;
+                                entityReference.UpdatedDate = DateTime.Now;
+                                break;
+                            }
+                    }
+                }
+            }
+
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var item in ChangeTracker.Entries()) 
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch(item.State)
+                    {
+                        case EntityState.Added:
+                            {
+                                entityReference.CreatedDate = DateTime.Now;
+                                break;
+                            }
+                            case EntityState.Modified:
+                            {
+                                Entry(entityReference).Property(x => x.CreatedDate).IsModified = false;
+                                entityReference.UpdatedDate = DateTime.Now; 
+                                break;
+                            }
+                    }
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //Git IEntityTypeConfiguration implemente eden tüm classlardaki configurasyonları al.
